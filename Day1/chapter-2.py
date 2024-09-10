e5fd7c35631bb58364922af60204bc96b6aeeca8
AnasRizwan25 <anasgame024@gmail.com>
2024-09-11 03:03:11 +0500

    Learning

---
+++ Day1/chapter-2.py
@@ -0,0 +1,36 @@
+# a = "hello"
+# b = "anas"
+# c = a+b
+# print(c)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# c = a + b
+
+# print(c)
+
+# practice Question
+
+# a , b = 1, 2
+# c = a + b
+# print(c)
+
+# a = 2
+# b = 4
+# reminder = a % b
+# print(reminder)
+
+# a = input("Check the type: ")
+# print(type(a))
+
+# a,b = 34,80
+# print(a>b)
+
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# averageNum = ( a + b ) / 2
+# print(averageNum)
+
+a = int(input("Enter the number: "))
+print(a**2)
---
+++ Day1/chapter-3.py
@@ -0,0 +1,25 @@
+# a = "anas is good boy"
+# # print(len(a))
+# # print(a.endswith("s"))
+# # print(a.startswith("a"))
+# b = a.replace("good", "bad")
+# print(b)
+
+# practice question
+
+# a = input("Enter your name: ")
+# print("Good Afternoon ",a.capitalize())
+
+# a = "anas"
+# b = "12-09-2024"
+
+# letter = '''Dear
+# <|Name|>,You are selected!
+# <|Date|>'''
+
+# print(letter.replace("<|Name|>","anas").replace("<|Date|>","02-10-2024"))
+
+# a = "hello  anas"
+# print(a.replace("  "," "))
+
+a = "Dear harry,\nthis python course is nice,\nThanks!"
